a117d5d33145b7de0661c23f5d9e86e495f5c106
Markus Andresen <markus.andresen94@gmail.com>
2018-09-10 13:35:03 +0200

    Take config as system argument

--- Main.py
+++ Main.py
@@ -1,15 +1,23 @@
 from NeuralFactory import neural_network_factory
 import random
 import tensorflow as tf
+import sys
+import os
 import numpy as np
+
+# Logging Verbosity
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 # Make program determenistic for testing purposes, ofc remove in production.
 random.seed(123)
 np.random.seed(123)
 tf.set_random_seed(123)
 
+
+config = sys.argv[1]
+
 # Create network and dataset from json config.
-network, cases = neural_network_factory("configs/segments.json")
+network, cases = neural_network_factory("configs/" + config + ".json")
 
 # Train model with casemanager.
 network.train_model(cases)
